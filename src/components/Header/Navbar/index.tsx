18c145896ecc1b62f06f015a0371f45e1d6d1395
ritik <ritikraw@gmail.com>
2023-10-29 19:52:51 +0530

    add active style in navigation

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,12 +1,39 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import styles from "./navbar.module.scss";
 
 function Navbar() {
+  const router = useRouter();
+  const pathName = usePathname();
+
   return (
     <ul className={styles.navbar}>
-      <li>Home</li>
-      <li>Songs</li>
-      <li>About</li>
+      <li
+        onClick={() => {
+          router.push("/");
+        }}
+        className={pathName === "/" ? styles.active : ""}
+      >
+        <a>Home</a>
+      </li>
+      <li
+        onClick={() => {
+          router.push("/songs");
+        }}
+        className={pathName === "/songs" ? styles.active : ""}
+      >
+        <a>Songs</a>
+      </li>
+      <li
+        onClick={() => {
+          router.push("/about");
+        }}
+        className={pathName === "/about" ? styles.active : ""}
+      >
+        <a>About</a>
+      </li>
     </ul>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React from "react";
 import styles from "./header.module.scss";
 import Navbar from "./Navbar";
 
 function Header() {
+  const router = useRouter();
   return (
     <div className={styles.container}>
-      <span className={styles.logo}>Reena Mehta</span>
+      <span className={styles.logo} onClick={() => router.push("/")}>
+        Reena Mehta
+      </span>
       <Navbar />
     </div>
   );
